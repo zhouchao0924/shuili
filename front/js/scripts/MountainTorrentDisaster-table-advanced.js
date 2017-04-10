95799567eec270deec4738684c3ed44deb6b6dc2
zhouchao <275869435@qq.com>
2017-04-10 14:51:55 +0800

    山洪灾害表格联调完成，缺导入导出

--- front/js/scripts/MountainTorrentDisaster-table-advanced.js
+++ front/js/scripts/MountainTorrentDisaster-table-advanced.js
@@ -1,7 +1,6 @@
-var MountainTorrentDisasterAdvanced = function() {
-	var initTable3 = function($scope, $compile) {
-		var table = $('#sample_3');
-		var conditions = [];
+var MountainTorrentDisasterAdvanced1 = function() {
+	var initTable = function($scope, $compile) {
+		var table = $('#sample_1');
 		var oTable = table.dataTable({
 			"language": {
 				"aria": {
@@ -38,38 +37,41 @@ var MountainTorrentDisasterAdvanced = function() {
 			"stateSave": true,
 			"serverSide": true,
 			"ajax": function(data, callback, settings) {
-				var a = JSON.parse(window.localStorage.Userdata);
 				var params = {
-					pageNo: data.start / data.length + 1,
-					pageSize: data.length,
-					conditions: conditions
+					page: data.start / data.length + 1,
+					text: $scope.text1
 				};
 				Metronic.blockUI({message: '<div style="background:rgba(0,0,0,0.3);padding:10px;font-size:16px;font-weight:bold;color:#fff;">正在加载...</div>', textOnly: true});
 				$.ajax({
-					url: Metronic.host + '/arter/queryArterList/' + a.userId + '/' + a.sessionId,
-					type: 'POST',
+					url: Metronic.host + 'flood/getBaseInfoList',
+					type: 'GET',
 					dataType: 'json',
-					data: JSON.stringify(params),
+					xhrFields: {
+						withCredentials: true
+					},
+					crossDomain: true,
+					data: {
+						data: JSON.stringify(params)
+					},
 					success: function(datas) {
-						console.debug(datas, '艺术家列表');
-						if (datas.code == 1) {
+						if (datas.success) {
 							var arr = [];
-							$.each(datas.obj.list || [], function(i, n) {
+							$.each(datas.data.list || [], function(i, n) {
 								var temp = [
 									n.id,
-									n.nickName,
-									n.displayArtType,
-									n.brief,
-									n.artProductCount,
+									n.manager,
+									n.village,
 									"",
-									n.artType
+									"",
+									"",
+									""
 								];
 								arr.push(temp);
 							});
 							var d = {
 								data: arr,
-								recordsTotal: datas.obj.totalRecords,
-								recordsFiltered: datas.obj.totalRecords
+								recordsTotal: datas.data.totalCount,
+								recordsFiltered: datas.data.totalCount
 							};
 							callback(d);
 							table.find('tbody tr td:last-child').each(function(i, n) {
@@ -96,27 +98,13 @@ var MountainTorrentDisasterAdvanced = function() {
 				});
 			}
 		});
-		var tableWrapper = $('#sample_3_wrapper');
-		$('#searchBtn').click(function(e) {
-			var nickName = $('#nickName').val(),
-				mobile = $('#mobile').val();
-			if (nickName) {
-				conditions.push({'type': 1, 'condition': nickName})
-			}
-			if (mobile) {
-				conditions.push({'type': 2, 'condition': mobile})
-			}
+		$('#searchBtn1').click(function(e) {
 			table.DataTable().ajax.reload();
-		});
-		$("#clear").click(function(e) {
-			$('#nickName').val('');
-			$('#mobile').val('');
-			conditions = [];
 		});
 		//添加enter搜索的事件
 		$('body').bind('keydown', function(e) {
 			if (e.which == 13) { //回车键的键值为13
-				$('#searchBtn').click();
+				$('#searchBtn1').click();
 			}
 		});
 	}
@@ -126,9 +114,447 @@ var MountainTorrentDisasterAdvanced = function() {
 			if (!jQuery().dataTable) {
 				return;
 			}
-			console.log('me 1');
-			initTable3($scope, $compile);
-			console.log('me 2');
+			initTable($scope, $compile);
+		}
+	};
+}();
+var MountainTorrentDisasterAdvanced2 = function() {
+	var initTable = function($scope, $compile) {
+		var table = $('#sample_2');
+		var oTable = table.dataTable({
+			"language": {
+				"aria": {
+					"sortAscending": ": activate to sort column ascending",
+					"sortDescending": ": activate to sort column descending"
+				},
+				"emptyTable": "表中无数据存在",
+				"info": "显示 _START_ 到 _END_ ，共 _TOTAL_ 条记录", //Showing _START_ to _END_ of _TOTAL_ entries
+				"infoEmpty": "", //No entries found
+				"infoFiltered": "", //(filtered1 from _MAX_ total entries)
+				"lengthMenu": "Show _MENU_ entries",
+				"search": "Search:",
+				"zeroRecords": "No matching records found"
+			},
+			"columnDefs": [
+				{
+					"orderable": false,
+					"targets": [0]
+				}
+			],
+			"order": [
+				[1, 'asc']
+			],
+			"lengthMenu": [
+				[
+					5, 15, 20, -1
+				],
+				[5, 15, 20, "All"] // change per page values here
+			],
+			// set the initial value
+			"pageLength": 30,
+			"lengthChange": false,
+			"filter": false,
+			"stateSave": true,
+			"serverSide": true,
+			"ajax": function(data, callback, settings) {
+				var params = {
+					page: data.start / data.length + 1,
+					text: $scope.text2
+				};
+				Metronic.blockUI({message: '<div style="background:rgba(0,0,0,0.3);padding:10px;font-size:16px;font-weight:bold;color:#fff;">正在加载...</div>', textOnly: true});
+				$.ajax({
+					url: Metronic.host + 'flood/getFloodLeaderInfoList',
+					type: 'GET',
+					dataType: 'json',
+					xhrFields: {
+						withCredentials: true
+					},
+					crossDomain: true,
+					data: {
+						data: JSON.stringify(params)
+					},
+					success: function(datas) {
+						if (datas.success) {
+							var arr = [];
+							$.each(datas.data.list || [], function(i, n) {
+								var temp = [
+									n.id,
+									n.post,
+									n.leader,
+									n.job,
+									n.cell,
+									n.uptime,
+									n.desc
+								];
+								arr.push(temp);
+							});
+							var d = {
+								data: arr,
+								recordsTotal: datas.data.totalCount,
+								recordsFiltered: datas.data.totalCount
+							};
+							callback(d);
+						} else {
+							alert(datas.message);
+							Metronic.unblockUI();
+						}
+					},
+					complete: function() {
+						Metronic.unblockUI();
+					}
+				});
+			}
+		});
+		$('#searchBtn2').click(function(e) {
+			table.DataTable().ajax.reload();
+		});
+		//添加enter搜索的事件
+		$('body').bind('keydown', function(e) {
+			if (e.which == 13) { //回车键的键值为13
+				$('#searchBtn2').click();
+			}
+		});
+	}
+	return {
+		//main function to initiate the module
+		init: function($scope, $compile) {
+			if (!jQuery().dataTable) {
+				return;
+			}
+			initTable($scope, $compile);
+		}
+	};
+}();
+var MountainTorrentDisasterAdvanced3 = function() {
+	var initTable = function($scope, $compile) {
+		var table = $('#sample_3');
+		var oTable = table.dataTable({
+			"language": {
+				"aria": {
+					"sortAscending": ": activate to sort column ascending",
+					"sortDescending": ": activate to sort column descending"
+				},
+				"emptyTable": "表中无数据存在",
+				"info": "显示 _START_ 到 _END_ ，共 _TOTAL_ 条记录", //Showing _START_ to _END_ of _TOTAL_ entries
+				"infoEmpty": "", //No entries found
+				"infoFiltered": "", //(filtered1 from _MAX_ total entries)
+				"lengthMenu": "Show _MENU_ entries",
+				"search": "Search:",
+				"zeroRecords": "No matching records found"
+			},
+			"columnDefs": [
+				{
+					"orderable": false,
+					"targets": [0]
+				}
+			],
+			"order": [
+				[1, 'asc']
+			],
+			"lengthMenu": [
+				[
+					5, 15, 20, -1
+				],
+				[5, 15, 20, "All"] // change per page values here
+			],
+			// set the initial value
+			"pageLength": 30,
+			"lengthChange": false,
+			"filter": false,
+			"stateSave": true,
+			"serverSide": true,
+			"ajax": function(data, callback, settings) {
+				var params = {
+					page: data.start / data.length + 1,
+					text: $scope.text3
+				};
+				Metronic.blockUI({message: '<div style="background:rgba(0,0,0,0.3);padding:10px;font-size:16px;font-weight:bold;color:#fff;">正在加载...</div>', textOnly: true});
+				$.ajax({
+					url: Metronic.host + 'flood/getFloodOwnerInfoList',
+					type: 'GET',
+					dataType: 'json',
+					xhrFields: {
+						withCredentials: true
+					},
+					crossDomain: true,
+					data: {
+						data: JSON.stringify(params)
+					},
+					success: function(datas) {
+						if (datas.success) {
+							var arr = [];
+							$.each(datas.data.list || [], function(i, n) {
+								var temp = [
+									n.id,
+									n.job,
+									n.village,
+									n.owner.name,
+									n.owner.post,
+									n.owner.cell,
+									n.desc
+								];
+								arr.push(temp);
+							});
+							var d = {
+								data: arr,
+								recordsTotal: datas.data.totalCount,
+								recordsFiltered: datas.data.totalCount
+							};
+							callback(d);
+						} else {
+							alert(datas.message);
+							Metronic.unblockUI();
+						}
+					},
+					complete: function() {
+						Metronic.unblockUI();
+					}
+				});
+			}
+		});
+		$('#searchBtn3').click(function(e) {
+			table.DataTable().ajax.reload();
+		});
+		//添加enter搜索的事件
+		$('body').bind('keydown', function(e) {
+			if (e.which == 13) { //回车键的键值为13
+				$('#searchBtn3').click();
+			}
+		});
+	}
+	return {
+		//main function to initiate the module
+		init: function($scope, $compile) {
+			if (!jQuery().dataTable) {
+				return;
+			}
+			initTable($scope, $compile);
+		}
+	};
+}();
+var MountainTorrentDisasterAdvanced4 = function() {
+	var initTable = function($scope, $compile) {
+		var table = $('#sample_4');
+		var oTable = table.dataTable({
+			"language": {
+				"aria": {
+					"sortAscending": ": activate to sort column ascending",
+					"sortDescending": ": activate to sort column descending"
+				},
+				"emptyTable": "表中无数据存在",
+				"info": "显示 _START_ 到 _END_ ，共 _TOTAL_ 条记录", //Showing _START_ to _END_ of _TOTAL_ entries
+				"infoEmpty": "", //No entries found
+				"infoFiltered": "", //(filtered1 from _MAX_ total entries)
+				"lengthMenu": "Show _MENU_ entries",
+				"search": "Search:",
+				"zeroRecords": "No matching records found"
+			},
+			"columnDefs": [
+				{
+					"orderable": false,
+					"targets": [0]
+				}
+			],
+			"order": [
+				[1, 'asc']
+			],
+			"lengthMenu": [
+				[
+					5, 15, 20, -1
+				],
+				[5, 15, 20, "All"] // change per page values here
+			],
+			// set the initial value
+			"pageLength": 30,
+			"lengthChange": false,
+			"filter": false,
+			"stateSave": true,
+			"serverSide": true,
+			"ajax": function(data, callback, settings) {
+				var params = {
+					page: data.start / data.length + 1,
+					text: $scope.text4
+				};
+				Metronic.blockUI({message: '<div style="background:rgba(0,0,0,0.3);padding:10px;font-size:16px;font-weight:bold;color:#fff;">正在加载...</div>', textOnly: true});
+				$.ajax({
+					url: Metronic.host + 'flood/getFloodNetworkOwnerInfoList',
+					type: 'GET',
+					dataType: 'json',
+					xhrFields: {
+						withCredentials: true
+					},
+					crossDomain: true,
+					data: {
+						data: JSON.stringify(params)
+					},
+					success: function(datas) {
+						if (datas.success) {
+							var arr = [];
+							$.each(datas.data.list || [], function(i, n) {
+								var temp = [
+									n.id,
+									n.village,
+									n.networkName,
+									n.networkType,
+									n.owner.name,
+									n.owner.post,
+									n.owner.cell,
+									n.desc
+								];
+								arr.push(temp);
+							});
+							var d = {
+								data: arr,
+								recordsTotal: datas.data.totalCount,
+								recordsFiltered: datas.data.totalCount
+							};
+							callback(d);
+						} else {
+							alert(datas.message);
+							Metronic.unblockUI();
+						}
+					},
+					complete: function() {
+						Metronic.unblockUI();
+					}
+				});
+			}
+		});
+		$('#searchBtn4').click(function(e) {
+			table.DataTable().ajax.reload();
+		});
+		//添加enter搜索的事件
+		$('body').bind('keydown', function(e) {
+			if (e.which == 13) { //回车键的键值为13
+				$('#searchBtn4').click();
+			}
+		});
+	}
+	return {
+		//main function to initiate the module
+		init: function($scope, $compile) {
+			if (!jQuery().dataTable) {
+				return;
+			}
+			initTable($scope, $compile);
+		}
+	};
+}();
+var MountainTorrentDisasterAdvanced5 = function() {
+	var initTable = function($scope, $compile) {
+		var table = $('#sample_5');
+		var oTable = table.dataTable({
+			"language": {
+				"aria": {
+					"sortAscending": ": activate to sort column ascending",
+					"sortDescending": ": activate to sort column descending"
+				},
+				"emptyTable": "表中无数据存在",
+				"info": "显示 _START_ 到 _END_ ，共 _TOTAL_ 条记录", //Showing _START_ to _END_ of _TOTAL_ entries
+				"infoEmpty": "", //No entries found
+				"infoFiltered": "", //(filtered1 from _MAX_ total entries)
+				"lengthMenu": "Show _MENU_ entries",
+				"search": "Search:",
+				"zeroRecords": "No matching records found"
+			},
+			"columnDefs": [
+				{
+					"orderable": false,
+					"targets": [0]
+				}
+			],
+			"order": [
+				[1, 'asc']
+			],
+			"lengthMenu": [
+				[
+					5, 15, 20, -1
+				],
+				[5, 15, 20, "All"] // change per page values here
+			],
+			// set the initial value
+			"pageLength": 30,
+			"lengthChange": false,
+			"filter": false,
+			"stateSave": true,
+			"serverSide": true,
+			"ajax": function(data, callback, settings) {
+				var params = {
+					page: data.start / data.length + 1,
+					text: $scope.text5
+				};
+				Metronic.blockUI({message: '<div style="background:rgba(0,0,0,0.3);padding:10px;font-size:16px;font-weight:bold;color:#fff;">正在加载...</div>', textOnly: true});
+				$.ajax({
+					url: Metronic.host + 'flood/getTransferPeopleList',
+					type: 'GET',
+					dataType: 'json',
+					xhrFields: {
+						withCredentials: true
+					},
+					crossDomain: true,
+					data: {
+						data: JSON.stringify(params)
+					},
+					success: function(datas) {
+						if (datas.success) {
+							var arr = [];
+							$.each(datas.data.list || [], function(i, n) {
+								var temp = [
+									n.id,
+									n.village,
+									n.dangerArea,
+									n.cat,
+									n.location.pos,
+									n.location.longitude,
+									n.location.latitude,
+									n.people.householder,
+									n.people.peopleCount,
+									n.people.cell,
+									n.transferOwner.name,
+									n.transferOwner.post,
+									n.transferOwner.cell,
+									n.transferLocation.posName,
+									n.transferLocation.secureVerify,
+									n.transferLocation.manager,
+									n.transferLocation.cell,
+									n.desc
+								];
+								arr.push(temp);
+							});
+							var d = {
+								data: arr,
+								recordsTotal: datas.data.totalCount,
+								recordsFiltered: datas.data.totalCount
+							};
+							callback(d);
+						} else {
+							alert(datas.message);
+							Metronic.unblockUI();
+						}
+					},
+					complete: function() {
+						Metronic.unblockUI();
+					}
+				});
+			}
+		});
+		$('#searchBtn5').click(function(e) {
+			table.DataTable().ajax.reload();
+		});
+		//添加enter搜索的事件
+		$('body').bind('keydown', function(e) {
+			if (e.which == 13) { //回车键的键值为13
+				$('#searchBtn5').click();
+			}
+		});
+	}
+	return {
+		//main function to initiate the module
+		init: function($scope, $compile) {
+			if (!jQuery().dataTable) {
+				return;
+			}
+			initTable($scope, $compile);
 		}
 	};
 }();
