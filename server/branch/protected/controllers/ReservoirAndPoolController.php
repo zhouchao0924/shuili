4ea0c3d8050bef3b91fe5efcff4bd517c2851cb3
lizhimin <lizhimin@tuniu.com>
2017-04-11 21:55:37 +0800

    添加日志

--- server/branch/protected/controllers/ReservoirAndPoolController.php
+++ server/branch/protected/controllers/ReservoirAndPoolController.php
@@ -14,7 +14,7 @@ class ReservoirAndPoolController extends Controller{
     public function actionImport(){
         $uploadInfo = $this->getUploadFileInfo();
         if(empty($uploadInfo)){
-            return $this->renderBadParamsAjaxResponse();
+            return $this->renderAjaxResponse($this->getAjaxResponse(false,"no file upload",ErrorCode::ERROR_COMMON_ERROR,array()));
         }
         $client = new ClientComponent();
         $userId = $client->getUserId();
