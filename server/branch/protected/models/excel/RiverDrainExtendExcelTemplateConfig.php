b66612e99cb18436b08244e15c4b6d099c380235
yulongfei <yulongfei@tuniu.com>
2017-04-15 12:14:11 +0800

    修改配置和文档

--- server/branch/protected/models/excel/RiverDrainExtendExcelTemplateConfig.php
+++ server/branch/protected/models/excel/RiverDrainExtendExcelTemplateConfig.php
@@ -15,8 +15,18 @@ class RiverDrainExtendExcelTemplateConfig extends ExcelTemplateConfig{
         'river_name'=>'排入水体名称',
         'pollution_source'=>'污染源',
         'village'=>'所在村',
-        'discharge_mode'=>'排放方式',
-        'in_river_mode'=>'入河方式',
+        'discharge_mode'=>array(
+            'lianxu'=>'连续',
+            'jianxian'=>'间歇',
+        ),
+        'in_river_mode'=>array(
+            'lianxu'=>'明渠',
+            'guandao'=>'管道',
+            'bangzhan'=>'泵站',
+            'hanzha'=>'涵闸',
+            'qianshe'=>'潜设',
+            'qita'=>'其他',
+        ),
         'treatment_method'=>'排水口处理方式',
         'remarks'=>'备注',
     );
