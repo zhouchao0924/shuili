375f79d18e8fad38fe21e23b43145dae94859759
lizhimin <lizhimin@tuniu.com>
2017-04-15 14:59:50 +0800

    调整excel兼容性

--- server/branch/protected/components/AuthDefine.php
+++ server/branch/protected/components/AuthDefine.php
@@ -32,11 +32,6 @@ class AuthDefine {
                     "childrenList"=>array(),
                 ),
                 array(
-                    "name"=>"地理信息",
-                    "url"=>"#/GEOInfo",
-                    "childrenList"=>array(),
-                ),
-                array(
                     "name"=>"水库山塘",
                     "url"=>"#/Reservoir",
                     "childrenList"=>array(),
