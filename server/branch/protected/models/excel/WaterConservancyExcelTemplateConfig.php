f2c231e707d50071c1f2034a673a95a92b255cbc
lizhimin <lizhimin@tuniu.com>
2017-04-15 20:32:49 +0800

    添加管理员照片

--- server/branch/protected/models/excel/WaterConservancyExcelTemplateConfig.php
+++ server/branch/protected/models/excel/WaterConservancyExcelTemplateConfig.php
@@ -9,21 +9,21 @@ class WaterConservancyExcelTemplateConfig extends ExcelTemplateConfig{
     public static $id = 3;
     protected $itemList = array(
         'name'=>'电站名称',
-	  'township'=>'所在乡镇',
-	  'basin'=>'流域',
+        'township'=>'所在乡镇',
+        'basin'=>'流域',
         'develop'=>"开发方式",
         'catchment_area'=>'集雨面积',
-	  'diversion'=>'发电引水',
-	  'head'=>'设计水头',
-	  'design_capacity'=>'设计流量',
-	  'installed_capacity'=>'装机容量',
-	  'plant_location'=>array(
-	  'jindu'=>'经度',
-	  'weidu'=>'纬度',
-  ),
-	  'commissionning_date'=>'投产年月',
-	  'reform'=>'改造情况',
-	  'ownership'=>'所有制',
+        'diversion'=>'发电引水',
+        'head'=>'设计水头',
+        'design_capacity'=>'设计流量',
+        'installed_capacity'=>'装机容量',
+        'plant_location'=>array(
+            'jindu'=>'经度',
+            'weidu'=>'纬度',
+        ),
+        'commissionning_date'=>'投产年月',
+        'reform'=>'改造情况',
+        'ownership'=>'所有制',
         'extend'=>'备注',
         'manager'=>array(
             'username'=>'姓名',
@@ -55,5 +55,13 @@ class WaterConservancyExcelTemplateConfig extends ExcelTemplateConfig{
     }
     protected function getTemplateName(){
         return "小水电";
+    }
+
+    public function getAdminImageColName(){
+        return "admin_image";
+    }
+
+    public function getAdminImageColAppendCol(){
+        return "manager";
     }
 }
